39ad63954b82b0e39783a52d229917986ae86cfc
raju-ahammad <web.rajuahammad@gmail.com>
2020-09-24 23:20:22 +0600

    create components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Navbar';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Wassup</h1>
-    </div>
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route path="/" />
+      </Switch>
+    </Router>
   );
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from "react-icons/fa";
+import { MdFingerprint } from 'react-icons/md';
+import { Link } from "react-router-dom";
+
 
 const Navbar = () => {
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
     return (
         <>
         <div className="navabr">
             <div className="navbar__container container">
                 <Link className="navbar__logo">
+                    <MdFingerprint className="navbar__icon" />
                     RAJU
                 </Link>
-                <div className="menu__icon">
-
+                <div className="menu__icon" onClick={handleClick}>
+                    {click ? <FaTimes/>: <FaBars/>  }
                 </div>
             </div>
         </div>
